209600ee15eecacd2e4a6612bc59b859d5ef3fb9
Denis Anuprienko <denis-anuprienko@yandex.ru>
2020-12-08 19:19:49 +0300

    Added constructor with default values

--- header.h
+++ header.h
@@ -5,32 +5,33 @@
 
 using namespace INMOST;
 
-class TwwoPhase
+class TwoPhaseFlow
 {
 private:
     // Physical parameters
-    double K0      = 1e-18;        // Initial intrinsic permeability, m^2
-    double mul     = 1.e-3;        // Liquid dynamic viscoity, Pa*s
-    double mug     = 1.e-3;        // Gas    dynamic viscoity, Pa*s
-    double rhol    = 7.0e2;        // Liquid density, kg/m^3
-    double rhog    = 7.0e2;        // Gas    density, kg/m^3
-    double rhos    = 2.0e3;        // Solid density, kg/m^3
-    double g       = 9.81;         // m/s^2
-    double c_f     = 1./22e9;      // parameter from van Noort and Yarushina, 1/Pa
-    double c_phi   = 9e-3*1e-6;    // parameter from van Noort and Yarushina, 1/Pa
-    double Pt      = 43e6;         // Confining pressure, Pa
-    double P0      = 1e5;          // Atmospheric pressure, Pa
-    double gamma   = 0.028*1e-6;   // Exponent factor for permeability function
-    double vg_a    = 1.0;          // van Genuchten pore parameter
-    double vg_n    = 5;            // van Genuchten pore parameter
-    double vg_m    = 1. - 1./vg_n;
-    double phi0    = 0.16;         // Initial porosity
+    double K0;     // Initial intrinsic permeability, m^2
+    double mul;    // Liquid dynamic viscoity, Pa*s
+    double mug;    // Gas    dynamic viscoity, Pa*s
+    double rhol;   // Liquid density, kg/m^3
+    double rhog;   // Gas    density, kg/m^3
+    double rhos;   // Solid density, kg/m^3
+    double g;      // m/s^2
+    double c_f;    // parameter from van Noort and Yarushina, 1/Pa
+    double c_phi;  // parameter from van Noort and Yarushina, 1/Pa
+    double Pt;     // Confining pressure, Pa
+    double P0;     // Atmospheric pressure, Pa
+    double gamma;  // Exponent factor for permeability function
+    double vg_a;   // van Genuchten pore parameter
+    double vg_n;   // van Genuchten pore parameter
+    double vg_m;
+    double phi0;   // Initial porosity
 
     // Numerical
     Mesh *mesh;
 
 public:
-    TwwoPhase(TwwoPhase)
+    TwoPhaseFlow();
+    ~TwoPhaseFlow();
 };
 
 #endif // HEADER_H
--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <cstdio>
 #include "header.h"
 
+TwoPhaseFlow::TwoPhaseFlow()
+{
+     K0      = 1e-18;
+     mul     = 1.e-3;
+     mug     = 1.e-3;
+     rhol    = 7.0e2;
+     rhog    = 7.0e2;
+     g       = 9.81;
+     c_f     = 1./22e9;
+     c_phi   = 9e-3*1e-6;
+     Pt      = 43e6;
+     P0      = 1e5;
+     gamma   = 0.028*1e-6;
+     vg_a    = 1.0;
+     vg_n    = 5;
+     vg_m    = 1. - 1./vg_n;
+     phi0    = 0.16;
+}
+
 int main(void)
 {
     printf("Hello world!\n");
