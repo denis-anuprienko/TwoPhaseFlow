cd68b73b8a2d5fa10502be2bfce64b56f832b727
Denis Anuprienko <denis-anuprienko@yandex.ru>
2021-09-14 20:14:30 +0300

    Improved pvtk2vtu

--- pvtk2vtu.py
+++ pvtk2vtu.py
@@ -10,6 +10,21 @@ N = 1
 
 for i in range(N):
     # create a new 'Partitioned Legacy VTK Reader'
-    pvtk = PartitionedLegacyVTKReader(registrationName='pvtk', FileName=dir+'sol'+str(i) +'.pvtk')
+    pvtk = PartitionedLegacyVTKReader(registrationName='pvtk', FileName=dir+'/sol'+str(i) +'.pvtk')
     # save data
-    SaveData(dir+'/psol'+str(i)+'.vtu', proxy=pvtk, PointDataArrays=[],CellDataArrays=['Liquid_Saturation'])
+    SaveData(dir+'/tmp.vtu', proxy=pvtk, PointDataArrays=[],CellDataArrays=['Liquid_Saturation'],DataMode='Binary',
+    CompressorType='LZMA',
+    CompressionLevel='9')
+    Delete(pvtk)
+    del pvtk
+    
+    tmp = XMLUnstructuredGridReader(registrationName='psol0.vtu', FileName=[dir+'/tmp.vtu'])
+    
+    tmp.CellArrayStatus = ['Liquid_Saturation']
+    tmp.PointArrayStatus = []
+
+    # save data
+    SaveData(dir+'/psol'+str(i)+'.vtu', proxy=tmp, CellDataArrays=['Liquid_Saturation'])
+    Delete(tmp)
+    del tmp
+    
